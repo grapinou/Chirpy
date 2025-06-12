9dc447d6597cd3bff3cac3fdd20c7a6151697e5c
sighto <remigrapinou@gmail.com>
2025-06-12 08:53:54 +0200

    starting server ok

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: mux,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println("Error server : ", err)
+	}
+
+}
